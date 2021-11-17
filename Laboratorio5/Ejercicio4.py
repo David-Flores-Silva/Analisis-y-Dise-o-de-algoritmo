16cae379b660b58280b18ec0f64eee4603b4ef31
David Flores Silva <dfloressi@unsa.edu.pe>
2021-11-17 09:04:33 -0500

    se agrego codigo del ejercicio4 del lab 5 , es una complejidad de n al cuadrado

---
+++ Laboratorio5/Ejercicio4.py
@@ -0,0 +1,20 @@
+#ejercicio4 - complejidad O(n^2)
+
+def complejidad_3(n):
+    #en este ejercicio vemos q la complejidad del codigo es n^2
+    #vemos que se realizan dos iteraciones for y cada una con O(n)
+    #O(n) * O(n) seria una complejidad de O(n^2)
+    
+    for i in range(n):          #------     #O(n)   #O(n)*O(n)      #O(n^2)
+        for j in range(n):      #------     #O(n)
+            print(j+1,"  |")          
+        print("-----\n")       
+    
+
+#Caso de prueba
+n = int(input("ingrese valor de n: "))
+complejidad_3(n)
+
+#O(n/2)
+#seria una complejidad de n/2
+
