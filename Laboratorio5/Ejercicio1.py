4906e0d0bd26e580287c3be2d49d499b589a8950
David Flores Silva <dfloressi@unsa.edu.pe>
2021-11-17 08:33:44 -0500

    iniciando laboratorio numero 5 y el primer ejercicio

---
+++ Laboratorio5/Ejercicio1.py
@@ -0,0 +1,15 @@
+#ejercicio1 - complejidad lineal
+
+def complejidad_1(n):
+    #en este ejercicio vemos q la complejidad del codigo es lineal en n 
+    #tiene que terminar de recorrer hasta el valor de n
+    for i in range(n):
+        print(i+1)
+    
+
+#Caso de prueba
+complejidad_1(5)
+
+#O(n)
+#seria una complejidad lineal de n, en este caso de 5
+
