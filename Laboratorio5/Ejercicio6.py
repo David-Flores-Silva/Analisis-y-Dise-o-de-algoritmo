02894e15ef1370cd17b735ee1e4e6e68b3d3bdfd
David Flores Silva <dfloressi@unsa.edu.pe>
2021-11-18 13:05:41 -0500

    se completo el codigo del ejercicio 6 del laboratorio 5, complejidad raiz de n

--- Laboratorio5/Ejercicio6.py
+++ Laboratorio5/Ejercicio6.py
@@ -1,18 +1,19 @@
-#ejercicio6 - complejidad O(n(n+1)/2)
+#ejercicio6 - complejidad O(n^½)
 
 def complejidad_6(n):
-    #en este ejercicio vemos q la complejidad del codigo es n(n+1)/2
+    #en este ejercicio vemos q la complejidad del codigo es n^½
     #es un caso especial ya que la segunda iteracion sera realizada
     #respecto del valor de i (primera iteracion) que ira aumentando
     #cada vez que este en el primer iterador
     
-    i=1
-    for p in range(n+1):
-        print("-----\n")       
-    
+    p = 0
+    i = 1
+    while p < n:        #p(p+1)/2
+        p += i           # P^2 = n 
+        i += 1           # P = n^½
+        print(p)
 
 #Caso de prueba
 n = int(input("ingrese valor de n: "))
 complejidad_6(n)
-
 
