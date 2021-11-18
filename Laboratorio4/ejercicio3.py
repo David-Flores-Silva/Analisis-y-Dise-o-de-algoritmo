e1f52916d2a6510868e1c2f428eca0193ad55323
David Flores Silva <dfloressi@unsa.edu.pe>
2021-11-18 16:36:45 -0500

    se modifco y funciono el codigo del tercer ejercicio del laboratorio 4

--- Laboratorio4/ejercicio3.py
+++ Laboratorio4/ejercicio3.py
@@ -1,7 +1,19 @@
-def twoSum(array):
-    for i in len(array):
-        for e in len(array):
-            if i != e & array[i] + array[e] == 10:
-                return True
+# Ejercicio3 - Pide hallar la complejidad del codigo
 
+def twoSum(array):
+    # El algoritmo recorre y siesq encuentra en el array
+    # dos numeros que sumados den 10 retorna True caso contrario retorna False
+    
+    for i in range(len(array)):                         # O(n)
+        for j in range(len(array)):                     # O(n)
+            if i != j and array[i] + array[j] == 10:
+                return True
     return False
+
+# RESPUESTA - COMPLEJIDAD
+# O(n^2)
+
+# Caso de prueba
+# array = [5, 6, 1, 7 , 2, 7] # False
+array = [5, 6, 1, 7 , 2, 4]  # True
+print(twoSum(array))
