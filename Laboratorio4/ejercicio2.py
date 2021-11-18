2c8d745593800c204b16e576a950193f8cc4220e
David Flores Silva <dfloressi@unsa.edu.pe>
2021-11-18 16:29:31 -0500

    se modifco y funciono el codigo del segundo ejercicio del laboratorio 4

--- Laboratorio4/ejercicio2.py
+++ Laboratorio4/ejercicio2.py
@@ -1,7 +1,26 @@
+# Enunciado: Pide hallar la complejidad del codigo
+import random
+
+
 def every_other(array):
-    array.each_with_index do |number, index|
+    # El algoritmo se hace que cuando el index es igual al numero 
+    # se debe de agregar un nuevo numero    
+    
+    for i in range(len(array)):
+        if i == array[i]:
+            array.append(random.randrange(0, 5))
+    
+    return array
+    
+    #Complejidad O(n)
+    
+    """array.each_with_index do |number, index|
         if index.even?
             array.each do |other_number|
-                puts number + other_number
+                puts number + other_number"""
+                
+array = every_other([0, 5, 6, 7, 2, 0, 4])
+print(array)
+          
 
                 
