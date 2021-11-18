34e9cdbc14051efe335239387bab88945fccc448
David Flores Silva <dfloressi@unsa.edu.pe>
2021-11-18 15:59:09 -0500

    se modifco y funciono el codigo del primer ejercicio del laboratorio 4

--- Laboratorio4/ejercicio1.py
+++ Laboratorio4/ejercicio1.py
@@ -1,11 +1,22 @@
+#Ejercicio 1 - laboratorio 4
 def greatestNumber(array):
-    for i in array:
+    #convertir el codigo de complejida O(n^2) a O(n)
+    """for i in array:
         isIValTheGreatest = True
         for e in array:
             if e > i:
                 isIValTheGreatest = False
         if isIValTheGreatest:
-            return i
+            return i"""
+    #Lo que nosotros buscamos es obtener el numero mat¿yor del arreglo
+    
+    m = 0
+    for i in range(len(array)):
+        if array[i] > m:
+            m = array[i]           
+    return m
 
-
+# Caso de prueba
+array = [0, 1, 2, 3, 4]
+print("Mayor: ", greatestNumber(array))
             
