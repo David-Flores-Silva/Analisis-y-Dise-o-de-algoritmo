37b9be722fc9491f0c342b1f81fed860a765a60b
David Flores Silva <dfloressi@unsa.edu.pe>
2021-12-15 11:26:11 -0500

    se creo los archivos para cada ejercicio

---
+++ Fase3-Lab10/Ejercicio3.py
@@ -0,0 +1,35 @@
+#https://cses.fi/problemset/task/1672
+#title
+
+def funcionde():
+    #decripcion
+    pass
+
+#pruebas
+
+
+#---------------------------------
+
+''' Input:
+4 3 5
+1 2 5
+1 3 9
+2 3 3
+1 2
+2 1
+1 3
+1 4
+3 2
+
+Output:
+5
+5
+8
+-1
+3 '''
+
+#-----------------------
+
+#Hay n ciudades y m carreteras entre ellas. Tu tarea es procesar consultas
+# q en las que tienes que determinar la longitud de la ruta más corta entre 
+# dos ciudades determinadas.
---
+++ Fase3-Lab10/Ejercicio4.py
@@ -0,0 +1,32 @@
+#https://cses.fi/problemset/task/1680
+#title
+
+def funcionde():
+    #decripcion
+    pass
+
+#pruebas
+
+
+#---------------------------------
+
+''' Input:
+5 5
+1 2
+2 5
+1 3
+3 4
+4 5
+
+Output:
+4
+1 3 4 5 '''
+
+#-----------------------
+
+#Uolevi ha ganado un concurso, y el premio es un vuelo gratuito que puede 
+# consistir en uno o más vuelos por ciudades. Por supuesto, Uolevi quiere 
+# elegir un viaje que tenga tantas ciudades como sea posible.
+# Uolevi quiere volar de Syrjälä a Lehmälä para visitar el número máximo de ciudades. 
+# Se le da la lista de vuelos posibles y sabe que no hay ciclos dirigidos en la red
+# de vuelos.
