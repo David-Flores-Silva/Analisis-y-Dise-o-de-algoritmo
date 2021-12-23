5a0d38261838da464da6e2258ca3759c5d525253
David Flores Silva <dfloressi@unsa.edu.pe>
2021-12-23 13:43:27 -0500

    solucion del ejercicio 4 incluido

--- Fase3-Lab11/Ejercicio4.java
+++ Fase3-Lab11/Ejercicio4.java
@@ -1,6 +1,35 @@
 //#https://cses.fi/problemset/task/1732
 //#title
 
+import java.util.*;
+public class Ejercicio4 {
+	public static void main(String [] args) {
+		Scanner tecla = new Scanner(System.in);
+		//String cadena = "abcababcab";
+		String cadena = tecla.next();
+		System.out.println(solucion(cadena));
+	}
+	
+	public static String solucion(String cad) {
+		String salida = "";
+		
+		if(cad.length() >= 1000000) {
+			return "(empty)";
+		}
+		
+		for(int i=0; i<cad.length()-1; i++) {
+			String a = cad.substring(0, i+1);
+			String b = cad.substring(cad.length()-(i+1), cad.length());
+			
+			if(a.equals(b)) {
+				salida += a.length() + " "; 
+			}
+		}
+
+		return salida;
+	}
+}
+
 /*
 #---------------------------------
 
